349a3c9a7eac781000fe7d342defc534b266655e
MIlicaMihajlovic <m.mihajlovicc@yahoo.com>
2018-11-27 18:56:33 +0100

    Car component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <nav class="nav">
+        <a class="nav-link active" href="/cars">Cars</a>
+        <!-- <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link disabled" href="#">Disabled</a> -->
+      </nav>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <router-view></router-view>
   </div>
 </template>
 
